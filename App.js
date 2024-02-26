00ecf8b7fd549a0bdcd717b7a586deb2898908ec
Akhilraj Akula <akulaakhilraj@gmail.com>
2024-02-26 15:20:31 -0500

    Akhil React 0224/1

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent=React.createElement("div", {id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{id:"heading"},"Hello Welcome to Poker Gaming Industry"),
+React.createElement("h1",{id:"heading"},"Hello Welcome to Poker run one Table ")]
+))
+
+const root1=ReactDOM.createRoot(document.getElementById("React"))
+root1.render(parent)
+
+console.log(parent)
